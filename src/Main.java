9e013f2f483f88061e6fb01ff202c78843386a47
Yuksel Argin <yukselargin@yahoo.com>
2022-10-10 16:32:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        int year,mod;
+        boolean isError= false;
+        String burc="";
+
+        System.out.print("Doğum yılınızı giriniz: ");
+        Scanner input= new Scanner(System.in);
+        year = input.nextInt();
+        mod = year%12;
+
+        switch(mod){
+            case 0:
+                burc="Maymun";
+                break;
+            case 1:
+                burc="Horoz";
+                break;
+            case 2:
+                burc="Köpek";
+                break;
+            case 3:
+                burc="Domuz";
+                break;
+            case 4:
+                burc="Fare";
+                break;
+            case 5:
+                burc="Öküz";
+                break;
+            case 6:
+                burc="Kaplan";
+                break;
+            case 7:
+                burc="Tavşan";
+                break;
+            case 8:
+                burc="Ejderha";
+                break;
+            case 9:
+                burc="Yılan";
+                break;
+            case 10:
+                burc="At";
+                break;
+            case 11:
+                burc="Koyun";
+                break;
+            default:
+                isError=true;
+        }
+        if(isError==true) {
+            System.out.print("Hatalı giriş yaptınız");
+        }else{
+            System.out.print("Çin Zodyağı Burcunuz: " + burc);
+        }
+    }
+}
